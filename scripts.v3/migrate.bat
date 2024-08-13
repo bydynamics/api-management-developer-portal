d0ea1dccf55de9bf193536b2476b7c3a5061fb52
Aldo van Amersfoort <aldo.vanamersfoort@bydynamics.com>
2024-08-13 14:09:22 +0200

    Changes to be committed: 	modified:   scripts.v3/migrate.bat

--- scripts.v3/migrate.bat
+++ scripts.v3/migrate.bat
@@ -1,10 +1,10 @@
 @REM This script automates content migration between developer portal instances.
 @REM Make sure you're logged-in with `az login` command before running the script.
 
-node ./migrate ^
---sourceSubscriptionId "< your subscription ID >" ^
---sourceResourceGroupName "< your resource group name >" ^
---sourceServiceName "< your service name >" ^
---destSubscriptionId "< your subscription ID >" ^
---destResourceGroupName "< your resource group name >" ^
---destServiceName "< your service name >"
+node ./scripts.v3/migrate ^
+--sourceSubscriptionId "33e31d54-2bef-4f17-9ae4-55aa291c7ce3" ^
+--sourceResourceGroupName "vanhappencontainersd" ^
+--sourceServiceName "vanhappencontainersd" ^
+--destSubscriptionId "33e31d54-2bef-4f17-9ae4-55aa291c7ce3" ^
+--destResourceGroupName "vanhappencontainersa" ^
+--destServiceName "vanhappencontainersa"
